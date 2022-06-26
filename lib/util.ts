45d6b50efd61f07b0431afc7dc78faaa07b657a1
Kevin Batdorf <kbat82@gmail.com>
2022-06-26 00:04:07 -0400

    emove delimiter

--- lib/util.ts
+++ lib/util.ts
@@ -1,5 +1,5 @@
 import type { IncomingHttpHeaders } from 'http'
 
 export const makeSessionName = (headers: IncomingHttpHeaders) => {
-    return [headers['user-agent'], headers['host']].filter(Boolean).join('^_^')
+    return [headers['user-agent'], headers['host']].filter(Boolean).join(' ')
 }
