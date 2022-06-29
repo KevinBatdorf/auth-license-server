2e05a5cb3dc76becbfb48109dadea151bf6cd1ae
Kevin Batdorf <kbat82@gmail.com>
2022-06-29 10:18:24 -0400

    stub out more flow comments

--- pages/create-password.tsx
+++ pages/create-password.tsx
@@ -1,1 +1,8 @@
 // View with a form to set a password and confirm it
+
+// This route requires a token in the query string
+// use edge middleware to detect (and confirm?) the token
+
+// type twice
+// password policy?
+// send comfirm email
--- pages/reset-password.tsx
+++ pages/reset-password.tsx
@@ -1,1 +1,5 @@
 // A view to enter their email and email them with a token
+
+// Keep messaging consistent
+// Keep response time consistent (maybe couple a promise.all with a setTimeout)
+// Rate limit the form submissions
